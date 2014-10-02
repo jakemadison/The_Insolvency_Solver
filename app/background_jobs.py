c364f25242fb0f9f6a154be61cc934c465c10738
Jake Madison <jakemadison@gmail.com>
2014-10-01 18:40:16 -0700

    added entry points for manually setting balance, and updating balance by the daily amt.

---
+++ app/background_jobs.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+from controller import get_current_rates, update_rates
+
+
+def daily_increase():
+    current_rates = get_current_rates()
+    daily_rate = current_rates.get('daily', False)
+
+    if daily_rate:
+        current_rates['balance'] += daily_rate
+        update_rates(current_rates)
+
+
+def _set_direct_balance(amt):
+    current_rates = get_current_rates()
+    current_rates['balance'] = amt
+    update_rates(current_rates)
+
+
+if __name__ == "__main__":
+    # daily_increase()
+    _set_direct_balance(30)
