8448020ee36b53f775c21250258dfd8065664280
Jake Madison <jakemadison@gmail.com>
2014-10-17 08:30:45 -0700

    added legend to stacked chart.  God damn this beast needs a refactoring.

--- app/static/js/d3_functions.js
+++ app/static/js/d3_functions.js
@@ -526,6 +526,25 @@ function create_transaction_plot(t_indicator, plot_style) {
               .style("fill", function (d) { return color(d.name); })
               .style("stroke", "grey");
 
+        var legend = svg.selectAll(".legend")
+              .data(color.domain().slice().reverse())
+            .enter().append("g")
+              .attr("class", "legend")
+              .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+          legend.append("rect")
+              .attr("x", width + 30)
+              .attr("width", 18)
+              .attr("height", 18)
+              .style("fill", color);
+
+          legend.append("text")
+              .attr("x", width + 24)
+              .attr("y", 9)
+              .attr("dy", ".35em")
+              .style("text-anchor", "end")
+              .text(function(d) { return d; });
+
 
     }
 
