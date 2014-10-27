3809ea459c4a0f033c736fb0d26db33d90b12e5e
Jake Madison <jakemadison@gmail.com>
2014-10-26 18:54:34 -0700

    human garbage.

--- app/static/js/d3_functions.js
+++ app/static/js/d3_functions.js
@@ -1,17 +1,4 @@
-//function daysBetween( date1, date2 ) {
-//  //Get 1 day in milliseconds
-//  var one_day=1000*60*60*24;
-//
-//  // Convert both dates to milliseconds
-//  var date1_ms = date1.getTime();
-//  var date2_ms = date2.getTime();
-//
-//  // Calculate the difference in milliseconds
-//  var difference_ms = date2_ms - date1_ms;
-//
-//  // Convert back to days and return
-//  return Math.round(difference_ms/one_day);
-//}
+
 function get_filter_list() {
 
         var filter_list = [];
@@ -855,11 +842,6 @@ function create_transaction_plot(t_indicator, plot_style) {
                         console.log(global_data.daily_summary[i].day, i, global_data.daily_summary.length);
                         i ++;
                     } while (global_data.daily_summary[i].day === end_date);
-
-
-
-
-
                 }
 
             }
