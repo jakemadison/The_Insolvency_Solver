d13fcb3e80eefb6645ca42a491e95d277b71a8c5
Jake Madison <jakemadison@gmail.com>
2014-11-19 09:07:40 -0800

    maybe basedir?

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-
 import os
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
@@ -19,7 +18,7 @@ def setup_logger(logger_instance):
     ch.setFormatter(formatter)
     logger_instance.addHandler(ch)
 
-    ch = logging.FileHandler('./insolvency_logger.log')
+    ch = logging.FileHandler(basedir+'/insolvency_logger.log')
     ch.setLevel(logging.INFO)
     ch.setFormatter(formatter)
     logger.addHandler(ch)
