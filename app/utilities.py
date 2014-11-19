ea33674a6fd310a2122157736d4fded3b73b08fd
Jake Madison <jakemadison@gmail.com>
2014-11-19 09:00:33 -0800

    maybe this help with logging?

--- app/utilities.py
+++ app/utilities.py
@@ -1,6 +1,11 @@
 from __future__ import print_function
 from datetime import datetime
 from app import basedir
+import logging
+from app import setup_logger
+logger = logging.getLogger(__name__)
+setup_logger(logger)
+logger.setLevel(logging.INFO)
 
 
 def get_recent_commit():
@@ -17,6 +22,7 @@ def get_recent_commit():
         translated_date = datetime.fromtimestamp(int(date)).strftime('%Y-%m-%d %H:%M:%S')
 
     except Exception, e:
+        logger.error(e)
         translated_date = 'unknown'
 
     return translated_date
